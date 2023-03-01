189d8acc488ec5b0cbc2b5882ce5f332ef1f7ea8
Luke Chamberland <luke.chamberland@shaw.ca>
2023-03-01 18:22:11 +0000

    implement final post route

--- express_server.js
+++ express_server.js
@@ -60,4 +60,9 @@ app.get("/u/:id", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id]
   res.redirect("/urls")
+});
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL
+  res.redirect("/urls")
 })
