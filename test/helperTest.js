2900f9eb9deb0d6ba922b1026dd1e43685fc9e01
Luke Chamberland <luke.chamberland@shaw.ca>
2023-03-10 20:43:53 +0000

    lint all

--- test/helperTest.js
+++ test/helperTest.js
@@ -17,13 +17,13 @@ const testUsers = {
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail( testUsers, "user@example.com")
+    const user = getUserByEmail( testUsers, "user@example.com");
     const expectedUserID = "userRandomID";
     assert.equal(user.email, "user@example.com");
     assert.equal(user.id, expectedUserID);
   });
   it('should return undefined of a non existant user', function() {
-    const user = getUserByEmail( testUsers, "a@b.c")
+    const user = getUserByEmail( testUsers, "a@b.c");
     assert.equal(user, undefined)
   });
 });
